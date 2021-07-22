0a24f2e6791f24820c63d6a493ee7f438a34c3ef
phin 801 <phinease456@gmail.com>
2021-07-22 09:20:20 -0600

    finished

--- 00-MovingSmile/MovingSmile.py
+++ 00-MovingSmile/MovingSmile.py
@@ -6,31 +6,45 @@ def main():
     pygame.init()
     pygame.display.set_caption("Moving Smile")
     screen = pygame.display.set_mode((640, 480))
-
+    eye_x = 0
+    eye_y = 0
+    clock = pygame.time.Clock()
     while True:
         # TODO 7: Set the clock speed to 60 fps
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             # TODO 4: Make a print statement for Up, Down, Left, and Right keys
             # TODO 5: Make the eye pupils move with Up, Down, Left, and Right keys
 
+
+        pressed_keys=pygame.key.get_pressed()
+        if pressed_keys [pygame.K_w]:
+            eye_y = eye_y -5
+        if pressed_keys[pygame.K_s]:
+            eye_y = eye_y +5
+        if pressed_keys[pygame.K_a]:
+            eye_x = eye_x -5
+        if pressed_keys[pygame.K_d]:
+            eye_x = eye_x +5
         screen.fill((255, 255, 255))  # white
-
-        # TODO 6: Make the eye pupils while holding down the key
-
         # API --> pygame.draw.circle(screen, color, (x, y), radius, thickness)
+
+
+
 
         pygame.draw.circle(screen, (255, 255, 0), (320, 240), 210)  # yellow circle
         pygame.draw.circle(screen, (0, 0, 0), (320, 240), 210, 4)  # black outline
 
         pygame.draw.circle(screen, (225, 225, 225), (240, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (240, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (242, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (242 + eye_x, 162 + eye_y), 7)  # black pupil
 
         pygame.draw.circle(screen, (225, 225, 225), (400, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (400, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (398, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (398 + eye_x, 162 + eye_y), 7)  # black pupil
 
         # TODO 1: Draw a nose
         # Suggestion: color (80,0,0) location (320,245), radius 10
