30789c713bff651b9a34ce7ff783e86487a2b94f
phin 801 <phinease456@gmail.com>
2021-07-22 09:45:46 -0600

    finished beging tests

--- 00-MovingSmile/MovingSmile.py
+++ 00-MovingSmile/MovingSmile.py
@@ -9,7 +9,10 @@ def main():
     eye_x = 0
     eye_y = 0
     clock = pygame.time.Clock()
+
     while True:
+        eye_1_cords = 242 + eye_x, 162 + eye_y
+        eye_2_cords = 398 + eye_x, 162 + eye_y
         # TODO 7: Set the clock speed to 60 fps
         clock.tick(60)
 
@@ -31,7 +34,8 @@ def main():
             eye_x = eye_x +5
         screen.fill((255, 255, 255))  # white
         # API --> pygame.draw.circle(screen, color, (x, y), radius, thickness)
-
+        if eye_1_cords [(pygame.color)(0,0,0)]:
+            print("pog")
 
 
 
@@ -40,11 +44,11 @@ def main():
 
         pygame.draw.circle(screen, (225, 225, 225), (240, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (240, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (242 + eye_x, 162 + eye_y), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (eye_1_cords), 7)  # black pupil
 
         pygame.draw.circle(screen, (225, 225, 225), (400, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (400, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (398 + eye_x, 162 + eye_y), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (eye_2_cords), 7)  # black pupil
 
         # TODO 1: Draw a nose
         # Suggestion: color (80,0,0) location (320,245), radius 10
